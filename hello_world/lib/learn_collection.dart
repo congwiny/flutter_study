094c7cdabeb0d880d4269eed66c227d19afcd167
niuchong <niuchong@360.cn>
2025-05-17 20:35:51 +0800

    Set的使用

--- hello_world/lib/learn_collection.dart
+++ hello_world/lib/learn_collection.dart
@@ -1,5 +1,21 @@
 void testCollection() {
-  _testList();
+  //_testList();
+  _testSet();
+}
+
+void _testSet() {
+  //Set<String> animals;
+  final animals = {'狗', '猫', '熊'};
+  //for in
+  for (var animal in animals) {
+    print(animal);
+  }
+
+  animals.add('猫');
+  //Set.forEach
+  animals.forEach((animal) {
+    print(animal);
+  });
 }
 
 //任何以 _ 开头的标识符，都是“库私有（library-private）”
