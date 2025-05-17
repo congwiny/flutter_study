abdf15aee018bf84bb9de26f871fc48512480f59
niuchong <niuchong@360.cn>
2025-05-17 19:32:14 +0800

    异常的简单使用

--- hello_world/lib/learn_dart.dart
+++ hello_world/lib/learn_dart.dart
@@ -8,4 +8,18 @@ void runDemo() {
 
   title = '小白兔开发';
   print('title=${title}');
+
+  getGasoline() {
+    return 0;
+  }
+
+  drive() {
+    var gasoline = getGasoline();
+    if (gasoline == 0) {
+      throw '没油了';
+    }
+    print('呜呜～');
+  }
+
+  drive();
 }
