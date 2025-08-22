4021e0cf44ce1f1e905bc5231065878df3e98bfb
niuchong <niuchong@360.cn>
2025-08-22 17:40:08 +0800

    面向对象： Person类的命名构造函数和初始化成员列表

--- hello_world/lib/oop/learn_oop.dart
+++ hello_world/lib/oop/learn_oop.dart
@@ -2,6 +2,9 @@ import 'models/person.dart';
 
 void testOop() {
   // 创建对象
-  Person person1 = Person('Alice', 25);
-  person1.introduce(); // 输出: Hi, I am Alice and I am 25 years old.
+  Person person1 = Person('Alice', 25, "Love Cat");
+  person1.introduce();
+
+  Person person2 = Person.fromBirthYear('Charlie', 1995, "Love Dog");
+  person2.introduce();
 }
--- hello_world/lib/oop/models/person.dart
+++ hello_world/lib/oop/models/person.dart
@@ -1,10 +1,20 @@
 class Person {
   String name;
   int age;
-  // 构造函数
-  Person(this.name, this.age);
+  String description;
+  // 默认构造函数
+  Person(this.name, this.age, this.description);
+
+  //命名构造函数
+  //this.name 直接给属性赋值
+  Person.fromBirthYear(this.name, int birthYear, String desc)
+    // 在初始化列表中赋值
+    : age = DateTime.now().year - birthYear,
+      description = desc.toUpperCase();
 
   void introduce() {
-    print('Hi, I am $name and I am $age years old.');
+    print(
+      'Hi, I am $name and I am $age years old. My description is $description',
+    );
   }
 }
