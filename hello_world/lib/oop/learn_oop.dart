a08b11cd13e577b0356bd4038491fe35c49e5d19
niuchong <niuchong@360.cn>
2025-08-22 17:46:55 +0800

    面向对象：Person命名构造函数的构造函数体

--- hello_world/lib/oop/learn_oop.dart
+++ hello_world/lib/oop/learn_oop.dart
@@ -2,7 +2,7 @@ import 'models/person.dart';
 
 void testOop() {
   // 创建对象
-  Person person1 = Person('Alice', 25, "Love Cat");
+  Person person1 = Person('Alice', 25, "Love Cat", ['Play']);
   person1.introduce();
 
   Person person2 = Person.fromBirthYear('Charlie', 1995, "Love Dog");
--- hello_world/lib/oop/models/person.dart
+++ hello_world/lib/oop/models/person.dart
@@ -2,19 +2,29 @@ class Person {
   String name;
   int age;
   String description;
+  List<String> hobbies;
+
   // 默认构造函数
-  Person(this.name, this.age, this.description);
+  Person(this.name, this.age, this.description, this.hobbies);
 
   //命名构造函数
   //this.name 直接给属性赋值
   Person.fromBirthYear(this.name, int birthYear, String desc)
     // 在初始化列表中赋值
     : age = DateTime.now().year - birthYear,
-      description = desc.toUpperCase();
+      description = desc.toUpperCase(),
+      hobbies = [] {
+    // 构造函数体：执行复杂逻辑
+    if (age < 18) {
+      hobbies.add('Studying');
+    } else {
+      hobbies.add('Working');
+    }
+  }
 
   void introduce() {
     print(
-      'Hi, I am $name and I am $age years old. My description is $description',
+      'Hi, I am $name and I am $age years old. Desc is $description Hobbies is $hobbies',
     );
   }
 }
