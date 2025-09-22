3b6789c3a949a9e2dd5ef6d6ad3d220194b4c4ab
niuchong <niuchong@360.cn>
2025-09-22 18:03:49 +0800

    RichText组件：建议优先使用Text.rich构造富文本

--- hello_world/lib/widget/rich_text.dart
+++ hello_world/lib/widget/rich_text.dart
@@ -37,6 +37,34 @@ class RichTextDemoPage extends StatelessWidget {
               maxLines: 1,
               overflow: TextOverflow.ellipsis,
             ),
+
+            SizedBox(height: 20),
+
+            //推荐使用Text.rich创建富文本
+            Text.rich(
+              TextSpan(
+                style: TextStyle(color: Colors.blue, fontSize: 18), // 父样式
+                children: [
+                  TextSpan(text: '普通文本 '), // 继承父级蓝色、18px
+                  TextSpan(
+                    text: '红色加粗 ',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    ), // 覆盖 color & weight，保留 fontSize
+                  ),
+                  TextSpan(
+                    text: '绿色大字',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 24,
+                    ), // 覆盖 color & fontSize
+                  ),
+                ],
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
           ],
         ),
       ),
