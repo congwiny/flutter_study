db05d945af47bddc601a40ef7f07e32d792f9c69
niuchong <niuchong@360.cn>
2025-11-14 18:35:20 +0800

    ListView组件：ListView.builder - 动态列表 适用于大量或无限滚动的子项。

--- hello_world/lib/widget/scroll/listview.dart
+++ hello_world/lib/widget/scroll/listview.dart
@@ -5,7 +5,7 @@ class ListViewExamplePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('ListView 示例')),
-      body: StaticListViewExample(),
+      body: DynamicListViewExample(),
     );
   }
 }
@@ -33,6 +33,34 @@ class StaticListViewExample extends StatelessWidget {
         ),
         // 可以继续添加更多静态子项
       ],
+    );
+  }
+}
+
+//适用于大量或无限滚动的子项。
+class DynamicListViewExample extends StatelessWidget {
+  final List<String> items = List.generate(100, (index) => 'Item $index');
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.blue,
+              child: Text('${index + 1}'),
+            ),
+            title: Text(items[index]),
+            subtitle: Text('This is item number $index'),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              print('Tapped on ${items[index]}');
+            },
+          ),
+        );
+      },
     );
   }
 }
